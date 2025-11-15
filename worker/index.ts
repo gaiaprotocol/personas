0ab8441a2f9b08ef48d5fbd7b1a141dea9b1c665
yj.gaia <gaiaprotocolnft@gmail.com>
2025-11-15 21:15:46 +0900

    Enhance worker configuration by adding ENV_TYPE variable for different environments and updating fetch handler to return environment type on request

--- worker/index.ts
+++ worker/index.ts
@@ -1,5 +1,9 @@
 export default {
   async fetch(request, env, ctx): Promise<Response> {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/envtype') return new Response(env.ENV_TYPE);
+
     return new Response('Hello World!');
 
     return new Response('Not Found', { status: 404 });
