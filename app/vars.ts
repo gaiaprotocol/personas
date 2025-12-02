4dce9feaf6d82824b6087a0f22a0896327516214
yj.gaia <gaiaprotocolnft@gmail.com>
2025-12-02 17:25:27 +0900

    Update PERSONA_FRAGMENTS_ADDRESS in vars.ts to new address for development environment

--- app/vars.ts
+++ app/vars.ts
@@ -3,4 +3,4 @@ export const MESSAGE_FOR_WALLET_LOGIN = 'Please sign this message to log in to G
 export const PERSONA_FRAGMENTS_ADDRESS =
   process.env.NODE_ENV === 'production' ?
     '0x' :
-    '0x5db776c722bc38ebd2a7354a7b51dd734c09d9fa';
+    '0x4720B04934c87388a7b9413413E2378EFF1D117C';
