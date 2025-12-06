056a35516e894a4adb5ce0a529b76457cbac6b8d
yj.gaia <gaiaprotocolnft@gmail.com>
2025-12-06 12:34:30 +0900

    Update persona fragments address for production environment and optimize bundle.js structure
    - Changed the persona fragments address in vars.ts for the production environment to a specific address.
    - Improved the structure of bundle.js for better readability and maintainability.

--- app/vars.ts
+++ app/vars.ts
@@ -2,5 +2,5 @@ export const MESSAGE_FOR_WALLET_LOGIN = 'Please sign this message to log in to G
 
 export const PERSONA_FRAGMENTS_ADDRESS =
   process.env.NODE_ENV === 'production' ?
-    '0x' :
+    '0xE59557cC3F9524Fa062120a02e467E1B07272688' :
     '0x4720B04934c87388a7b9413413E2378EFF1D117C';
